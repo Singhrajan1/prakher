d8d099c86f23087b2b555b1b1d19b73b16b85f8a
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-07-22 10:24:14 +0530

    prakher

---
+++ Advance_Java/example.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Mai2n{
+    public static void main(String[] args){
+        System.out.println("rajan kumar singh");
+        ArrayList<Integer> ar = new ArrayList<>();
+        int arr[] = {2,3,3,3,4};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
